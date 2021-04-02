802c8f99700f09aae4fabceb2d086e4f2fd87310
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-02 16:49:57 +0700

    Create PermissionLivewire.php

---
+++ src/Http/Livewire/PermissionLivewire.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Thotam\ThotamPermission\Http\Livewire;
+
+use Livewire\Component;
+
+class PermissionLivewire extends Component
+{
+    public function render()
+    {
+        return view('thotam-permission::livewire.permission-livewire');
+    }
+}
