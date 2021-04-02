8617efc2803b5232bfe04ba65025da743a5a0783
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-02 16:56:44 +0700

    Update filters.blade.php

--- resources/views/livewire/role/sub/filters.blade.php
+++ resources/views/livewire/role/sub/filters.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-auto mb-4 pr-md-3">
                 <label class="form-label d-none d-md-block">&nbsp;</label>
                 <div class="col px-0 mb-1 text-md-left text-center">
-                    <button type="button" class="btn btn-success waves-effect" wire:click="$emit('add_role')" wire:loading.attr="disabled"><span class="fas fa-plus-circle mr-2"></span>Thêm</button>
+                    <button type="button" class="btn btn-success waves-effect" wire:click.prevent="add_role" wire:loading.attr="disabled" thotam-blockui><span class="fas fa-plus-circle mr-2"></span>Thêm</button>
                 </div>
             </div>
         @endif
