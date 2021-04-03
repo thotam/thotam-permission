2df666aac66091f7836a8693f11ecd192e8b26ee
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-03 10:15:21 +0700

    Update add_edit.blade.php

--- resources/views/livewire/role/modal/add_edit.blade.php
+++ resources/views/livewire/role/modal/add_edit.blade.php
@@ -45,9 +45,9 @@
                                         <div id="group_div">
                                             <input type="text" class="form-control px-2" wire:model.debounce.500ms="group" id="group" style="width: 100%" placeholder="Nhóm Role ..." autocomplete="off"  list="suggestions">
                                             <datalist id="suggestions">
-                                                @if (!!$gruop_arrays)
-                                                    @foreach ($gruop_arrays as $gruop_array)
-                                                        <option value="{{ $gruop_array }}">
+                                                @if (!!$group_arrays)
+                                                    @foreach ($group_arrays as $group_array)
+                                                        <option value="{{ $group_array }}">
                                                     @endforeach
                                                 @endif
                                             </datalist>
