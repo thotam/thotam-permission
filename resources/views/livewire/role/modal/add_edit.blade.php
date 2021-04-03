c350995b32623fb51243264f0a0ad1a1b83a4cab
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-03 10:17:39 +0700

    Update add_edit.blade.php

--- resources/views/livewire/role/modal/add_edit.blade.php
+++ resources/views/livewire/role/modal/add_edit.blade.php
@@ -1,9 +1,9 @@
 <div wire:ignore.self class="modal fade" id="add_edit_modal" tabindex="-1" role="dialog" aria-labelledby="add_edit_modal" aria-hidden="true" data-toggle="modal" data-backdrop="static" data-keyboard="false">
 
-    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
         <div class="modal-content py-2">
             <div class="modal-header">
-                <h4 class="modal-title text-orange"><span class="fas fa-bullseye mr-3"></span>{{ $modal_title }}</h4>
+                <h4 class="modal-title text-purple"><span class="fas fa-user-tag mr-3"></span>{{ $modal_title }}</h4>
                 <button type="button" wire:click.prevent="cancel()" class="close" data-dismiss="modal" wire:loading.attr="disabled" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
