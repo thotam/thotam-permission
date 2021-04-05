13b17b7e5017ae4e4744aba6b558bbb78afb35fc
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-05 17:24:24 +0700

    Revert "Update role-livewire.blade.php"
    This reverts commit bcc5ed812e3c3f065a85177993bccdb9072b9877.

--- resources/views/livewire/role/role-livewire.blade.php
+++ resources/views/livewire/role/role-livewire.blade.php
@@ -5,16 +5,4 @@
 
     <!-- Incluce các modal -->
     @include('thotam-permission::livewire.role.modal.add_edit')
-
-    <!-- Scripts -->
-    @push('livewires')
-        <script>
-            document.addEventListener("DOMContentLoaded", () => {
-                Livewire.hook('component.initialized', (component) => {
-                    console.log(component);
-                    console.log(@this);
-                })
-            });
-        </script>
-    @endpush
 </div>
