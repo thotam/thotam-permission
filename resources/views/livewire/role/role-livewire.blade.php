c3bdcb364ed744401af9ef0e553656d3b3aaa452
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-06 14:03:04 +0700

    Update role-livewire.blade.php

--- resources/views/livewire/role/role-livewire.blade.php
+++ resources/views/livewire/role/role-livewire.blade.php
@@ -5,4 +5,14 @@
 
     <!-- Incluce các modal -->
     @include('thotam-permission::livewire.role.modal.add_edit')
+
+    <!-- Scripts -->
+    @push('livewires')
+        <script>
+            document.addEventListener("DOMContentLoaded", () => {
+                window.thotam_livewire = @this;
+                Livewire.emit("dynamic_update_method");
+            });
+        </script>
+    @endpush
 </div>
